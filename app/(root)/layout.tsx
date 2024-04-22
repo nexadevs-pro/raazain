a645fc5e0356f451d81059bf46511e88e57d44d7
darshit890 <88980439+darshit890@users.noreply.github.com>
2024-04-22 16:43:46 +0530

    modified:   app/(root)/layout.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 import "../globals.css";
 import Navbar from "@/components/Navbar";
@@ -25,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <SpeedInsights />
         <ClerkProvider>
         <Provider>
           <ToasterProvider />
