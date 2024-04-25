b095f5d72b04e4bf8b1717c2c1f178c31a749e71
darshit890 <88980439+darshit890@users.noreply.github.com>
2024-04-25 22:41:21 +0530

    modified:   components/ProductCard.tsx

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -27,7 +27,7 @@ const ProductCard = ({ product, updateSignedInUser }: ProductCardProps ) => {
   const cart = useCart();
   return (
     <Link key={product._id} href={`/products/${product._id}`}>
-    <Card className='flex justify-between cursor-pointer relative w-[212px] h-[380px]'>
+    <Card className='flex justify-between cursor-pointer relative w-[212px] h-[350px]'>
       <CardHeader>
         <Image width={300} height={300} src={product.media[0]} alt='product' className="w-[229px] h-[212px]" />
         <div className='absolute bottom-[40px] right-5'>
@@ -47,19 +47,17 @@ const ProductCard = ({ product, updateSignedInUser }: ProductCardProps ) => {
         </div>
       </CardHeader>
       <CardFooter className='flex flex-col items-start space-y-1 '>
-        <h1 className='text-sm line-clamp-1'>{product.title}</h1>
+        <h1 className='text-sm line-clamp-1 pb-1'>{product.title}</h1>
         <p className='text-primary font-medium'>Dhs. {product.price}</p>
-        <div className='flex flex-row  items-center justify-between'>
-          <div className='flex flex-row  items-center justify-between'>
-            <div className='flex flex-col items-center gap-x-2 pl-0.5'>
-              <h1 className='line-through pr-1 text-xs'>Dhs. 121</h1>
-              <p className='text-xs text-primary'>25% OFF</p>
+        <div className="flex flex-row justify-between items-center gap-x-12">
+        <div className="flex flex-row">
+          <h1 className='line-through pr-1  text-xs'>Dhs. 121</h1>
+          <p className='text-xs text-primary'>25% OFF</p>
+        </div>
+        <div className="flex flex-row items-center ">
+            <p>4.5</p>
+            <FaStar className='text-yellow-500' />
             </div>
-            <div className='ml-[6rem] mt-1 flex items-center gap-x-1'>
-              <p>4.5</p>
-              <FaStar className='text-yellow-500' />
-            </div>
-          </div>
         </div>
       </CardFooter>
     </Card>
