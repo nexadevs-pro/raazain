0f20b024dd469812f62c72ea0f5ef2e24277ca0a
darshit890 <88980439+darshit890@users.noreply.github.com>
2024-05-02 15:29:29 +0530

    modified:   lib/types.d.ts

--- components/Collections.tsx
+++ components/Collections.tsx
@@ -7,7 +7,7 @@ const Collections = async () => {
 
   return (
     <section className="my-10 max-w-[82rem] px-2 mx-auto">
-        <div className="grid lg:grid-cols-7 xl:grid-cols-9 md:grid-cols-6 sm:grid-cols-4 grid-cols-3  ">
+        <div className="grid lg:grid-cols-7 xl:grid-cols-9 md:grid-cols-6 sm:grid-cols-4 grid-cols-3 gap-x-4 ">
           {collections.map((collection: CollectionType) => (
             <Link href={`/collections/${collection._id}`} key={collection._id} className="flex flex-col justify-center items-center gap-y-2 mt-8 hover:scale-110">
               <Image
@@ -16,9 +16,9 @@ const Collections = async () => {
                 alt={collection.title}
                 width={100}
                 height={100}
-                className="rounded-full cursor-pointer"
+                className="rounded-full cursor-pointer w-[100px] h-[100px] mb-2 flex items-center justify-center"
               />
-              <p className="text-muted-foreground text-xs">{collection.title}</p>
+              <p className="text-muted-foreground text-sm line-clamp-1  text-center">{collection.title}</p>
             </Link>
           ))}
         </div>
