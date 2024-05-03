f921040615e563424d2605d8bc52d3932f9cab7a
mihirbhadak <98308594+mihirbhadak@users.noreply.github.com>
2024-05-04 00:02:07 +0530

    modified:   app/(root)/page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
       <ProductListOtwo />
       <ProductListCosrx />
       <ProductListOrdinary />
-      <CallToAction />
+      {/* <CallToAction /> */}
     </>
   );
 }
